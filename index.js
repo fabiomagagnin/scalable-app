06588895ebc394610f2f5c12d9f50f4871c27d94
Fabio Magagnin <fmagagnin@fullstacklabs.co>
2024-03-27 09:23:27 -0300

    return IP

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express')
+const consumeMemoryForDuration = require('./memory')
 const app = express()
+let counter = 0
 
 app.get('/', function (req, res) {
-  res.send('Scalable App')
+  res.send('Scalable App. IP: ' + req.ip)
 })
 
-app.get('/app/memory', function(req, res) {
+app.post('/', function (req, res) {
+  res.send('Scalable App. IP: ' + req.ip)
+})
+
+app.post('/app/memory', async function (req, res) {
+  try {
+    consumeMemoryForDuration(req.query.size, req.query.time)
+    counter++
     res.send('memory')
+  } catch (e) {
+    console.error(e)
+    res.send('error')
+  }
 })
 
-app.get('/app/cpu', function(req, res) {
-    res.send('cpu')
+app.get('/app/cpu', function (req, res) {
+  res.send('cpu')
 })
+
+// setInterval(() => { 
+//   gc()
+//   console.log('Calls: ', counter)
+// }, 5000)
 
 app.listen(3000)
+
+
