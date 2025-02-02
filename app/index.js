d8db6f2c3418ff74a82992bf30d3db745929a810
Fabio Magagnin <fmagagnin@fullstacklabs.co>
2025-02-02 11:02:33 -0300

    Testing app runner update

--- app/index.js
+++ app/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Scalable App. IP: ' + req.ip)
+  res.send('Scalable App. IP: ' + req.ip + ' - ' + new Date())
 })
 
 app.get('/hello', async function (req, res) {
-  res.json({hello: 'hello!'})
+  res.json({hello: 'hello!', time: new Date()})
 })
 
 app.listen(3000)
